1379f9a2d0908b32cafd12895d2d050272e72e9f
sheikhmuhammadshahid <112244414+sheikhmuhammadshahid@users.noreply.github.com>
2023-06-20 12:43:23 +0500

    done

---
+++ BiitSocioApis/Controllers/FriendsController.cs
@@ -0,0 +1,217 @@
+﻿using BiitSocioApis.classes;
+using BiitSocioApis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BiitSocioApis.Controllers
+{
+    public class FriendsController : ApiController
+    {
+        BIITSOCIOEntities db = new BIITSOCIOEntities();
+       
+
+        [HttpGet]
+        public HttpResponseMessage muteOrUnmuteGroup(int groupId, string userId, bool todo)
+        {
+            try
+            {
+                BIITSOCIOEntities db = new BIITSOCIOEntities();
+                if (todo)
+                {
+                    var ug = db.MutedGroups.Where(s => s.groupId == groupId && s.userId == userId).FirstOrDefault();
+                    if (ug != null)
+                    {
+                        db.MutedGroups.Remove(ug);
+                        db.SaveChanges();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, "Group Un-muted!");
+                }
+                else
+                {
+
+                    db.MutedGroups.Add(new MutedGroup { groupId = groupId, userId = userId });
+                    db.SaveChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, "Group muted!");
+                }
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage leaveGroup(int groupId, string userId)
+        {
+            try
+            {
+                BIITSOCIOEntities db = new BIITSOCIOEntities();
+                var ug = db.UserGroups.Where(s => s.groupId == groupId && s.userId == userId).FirstOrDefault();
+                if (ug != null)
+                {
+                    db.UserGroups.Remove(ug);
+                    db.SaveChanges();
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, "Group left!");
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+
+            }
+        }
+
+
+        [HttpPost]
+        public HttpResponseMessage sendFriendRequest(FriendRequest friend)
+        {
+            try
+            {
+
+               
+
+                bool isExist = db.FriendRequests.Any(s => s.RequestedTo == friend.RequestedTo && s.RequestedBy == friend.RequestedBy);
+                if (!isExist)
+                {
+                   
+                    FriendRequest f = new FriendRequest();
+                    f.status = friend.status;
+                    f.RequestedBy= friend.RequestedBy;
+                    f.RequestedTo= friend.RequestedTo;
+                    
+                    db.FriendRequests.Add(f);
+                    db.SaveChanges();
+                    notification n = new notification();
+                    n.notificationFrom = friend.RequestedBy;
+                    n.NotificationTo = friend.RequestedTo;
+                    n.title = "";
+                    n.dateTime = DateTime.Now.ToShortDateString();
+                    //n.status = "pending";
+                    n.status = 0;
+                    n.body = db.FriendRequests.OrderByDescending(s => s.id).Select(s => s.id.ToString()).FirstOrDefault();
+                    n.fromWall = friend.id;
+                    n.type = "request";
+                    db.notifications.Add(n);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, "sent successfully!");
+                }
+                else {
+                    return Request.CreateResponse(HttpStatusCode.OK, "Already requested!");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+            }
+
+        }
+        [HttpGet]
+        public HttpResponseMessage acceptFriendRequest(int reques_id,bool status,int noti_id)
+        {
+            try
+            {
+                var notification = db.notifications.Where(s => s.id == noti_id).FirstOrDefault();
+                if (notification != null)
+                {
+                    notification.status = 2;
+                    db.SaveChanges();
+                }
+                FriendRequest request= db.FriendRequests.Where(s=>s.id==reques_id).SingleOrDefault();
+                if (status)
+                    request.status = "accepted";
+                else
+                {
+                    request.status = "rejected";
+
+                }
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, ""+request.status+" successfully!");
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+            }
+
+        }
+        [HttpGet]
+        public HttpResponseMessage unfriend(string sentBy,string sentTo)
+        {
+            try
+            {
+
+                FriendRequest request = db.FriendRequests.Where(s => s.RequestedBy == sentBy && s.RequestedTo==sentTo).FirstOrDefault();
+                if (request != null)
+                {
+                    db.FriendRequests.Remove(request);
+                    db.SaveChanges();
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, "Removed successfully!");
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+            }
+
+        }
+
+        [HttpGet]
+        public HttpResponseMessage getFriends(string user_id)
+        {
+            try
+            {
+
+                List<String> request = db.FriendRequests.Where(s => s.status.ToLower()=="accepted"&&(s.RequestedBy.Trim() == user_id || s.RequestedTo.Trim() == user_id)).Select(s=>s.RequestedTo.Trim()==user_id?s.RequestedBy.Trim():s.RequestedTo.Trim()).ToList();
+                var res = (from us in db.Users join r in request on us.CNIC equals r select us).ToList(); 
+                return Request.CreateResponse(HttpStatusCode.OK, res);
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+            }
+
+        }
+
+        [HttpGet]
+        public HttpResponseMessage getTotalFriends(string user_id)
+        {
+            try
+            {
+
+               int count = db.FriendRequests.Where(s => s.RequestedBy.Trim() == user_id).ToList().Count;
+
+                return Request.CreateResponse(HttpStatusCode.OK, count);
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+            }
+
+        }
+        [HttpGet]
+        public HttpResponseMessage getFriendRequests(string user_id)
+        {
+            try
+            {
+
+                List<FriendRequest> requests = db.FriendRequests.Where(s => s.RequestedBy.Trim() == user_id && s.status!="Accepted").ToList();
+
+                return Request.CreateResponse(HttpStatusCode.OK, requests);
+            }
+            catch (Exception ex)
+            {
+                return new PostController().getExceptionMessage();
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ BiitSocioApis/classes/Server.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace BiitSocioApis.classes
+{
+    public class Serverr
+    {
+        public string buzzerPressedBy = "-1";
+        public List<CLientss> players = new List<CLientss>();
+
+
+        private Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local IP address and port
+        private IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+
+        private void bindd()
+        {
+            try
+            {
+                listener.Bind(localEndPoint);
+            }
+            catch (Exception ex) { }
+        }
+        string getConnectedList()
+        {
+            string data = "";
+            try
+            {
+                foreach (var item in players)
+                {
+                    data += item.id + "~";
+                }
+            }
+            catch (Exception ex) { }
+            return data;
+        }
+        public void StartListening()
+        {
+            try
+            {
+                bindd();
+                listener.Listen(2);
+                players.Clear();
+                buzzerPressedBy = "-1";
+                bool teamsDone = true;
+                for (int i = 0; true; i++)
+                {
+                    // Start listening for incoming connections
+
+                    // Accept an incoming connection
+                    Socket client = listener.Accept();
+
+                    new Thread(() => { handleClient(client); }).Start();
+
+                }
+                //MessageBox.Show("ended");
+            }
+            catch (Exception ex) { }
+        }
+        void handleClient(Socket socket)
+        {
+            try
+            {
+                CLientss c = new CLientss();
+                c.socket = socket;
+                byte[] data = new byte[1024];
+                int bytesReceived = socket.Receive(data);
+                string message = Encoding.ASCII.GetString(data, 0, bytesReceived);
+                string[] dataa = message.Split(':');
+                c.id = dataa[0];
+                c.name = dataa[1];
+                players.Add(c);
+                Console.WriteLine(message + " is conncted ,Total are " + players.Count);
+                new Thread(() =>
+                {
+                    new Thread(() => propagateMessage(getConnectedList(), c.socket, "-1")).Start();
+                    startListening(players[players.Count - 1].socket, players.Count - 1);
+                }).Start();
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        /* public static void setInitial(string tosend)
+         {
+             try
+             {
+
+                 //players[1].Send(Encoding.ASCII.GetBytes(tosend));
+                 //players[0].Send(Encoding.ASCII.GetBytes(tosend));
+                 if (players.Count == 2)
+                 {
+
+                     Thread t = new Thread(() => { players[0].Send(Encoding.ASCII.GetBytes(tosend)); });
+                     Thread t1 = new Thread(() => { players[1].Send(Encoding.ASCII.GetBytes(tosend)); });
+                     t.Start(); t1.Start();
+                     t.Join(); t1.Join();
+                     if (tosend == "Completed")
+                     {
+                         if (players.Count == 2)
+                         {
+                             players[0].Close();
+                             players[1].Close();
+                             players.Clear();
+                         }
+                     }
+                 }
+             }
+             catch (Exception ex) { }
+         }*/
+
+        void propagateMessage(string message, Socket to, string from)
+        {
+            try
+            {
+                if (from != "-1" && from != "-2" && to != null)
+                {
+                    message = "-0~" + from + "~" + message;
+                    Console.WriteLine(message);
+                    new Thread(() => to.Send(Encoding.ASCII.GetBytes(message))).Start();
+                    return;
+                }
+                else
+                {
+                    lock (players)
+                    {
+                        message = from + "~" + message;
+                        var data = Encoding.ASCII.GetBytes(message);
+                        foreach (var item in players)
+                        {
+                            try
+                            {
+                                new Thread(() => { item.socket.Send(data); }).Start();
+                            }
+                            catch (Exception ex) { }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        void startListening(Socket client, int index)
+        {
+            try
+            {
+                while (client.Connected)
+                {
+
+                    byte[] data = new byte[1024];
+                    int bytesReceived = client.Receive(data);
+                    string message = Encoding.ASCII.GetString(data, 0, bytesReceived);
+                    // Console.WriteLine(message);
+                    try
+                    {
+                        string[] data2 = message.Split('~');
+
+                        //byte[] data1 = Encoding.ASCII.GetBytes(data2[2]);
+                        string id = data2[1];
+                        new Thread(() => { propagateMessage(data2[2], players.Where(s => s.id == id).Select(s => s.socket).FirstOrDefault(), data2[0]); }).Start();
+                        //BuzzerRoundCompleted= true;
+
+                    }
+                    catch (Exception ex) { }
+
+
+                }
+                string idd = players[index].id;
+
+
+                players.RemoveAt(index);
+                Console.WriteLine(idd + " is Disconneted ,Remaining are " + players.Count);
+
+                propagateMessage(idd, null, "-2");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                try
+                {
+                    string id = players[index].id;
+
+
+                    players.RemoveAt(index);
+                    Console.WriteLine(id + " is Disconneted ,Remaining are " + players.Count);
+                    propagateMessage(id, null, "-2");
+                }
+                catch (Exception ecx)
+                {
+                    Console.WriteLine(ecx.Message);
+                }
+            }
+        }
+        public void getIP()
+        {
+            try
+            {
+                var wifiInterface = NetworkInterface.GetAllNetworkInterfaces()
+         .Where(i => i.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && i.Name.StartsWith("Wi"))
+         .FirstOrDefault();
+
+                // Get the IPv4 address of the WiFi interface
+                IPAddress ipAddress = wifiInterface.GetIPProperties().UnicastAddresses
+                    .Where(a => a.Address.AddressFamily == AddressFamily.InterNetwork)
+                    .Select(a => a.Address)
+                    .FirstOrDefault();
+                Console.WriteLine("Listening at: " + ipAddress.ToString());
+
+            }
+            catch
+            {
+                Console.WriteLine("Try Again!");
+
+            }
+
+        }
+    }
+    public class CLientss
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Socket socket { get; set; }
+    }
+}
